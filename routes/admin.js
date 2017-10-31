93b7c6fcbf23b3c672fe467ce454b51d2cc6ff89
bader fahoum <fahoumbader@gmail.com>
2017-10-31 15:41:21 +0200

    Update admin.js

--- routes/admin.js
+++ routes/admin.js
@@ -7,6 +7,17 @@ var middleware  = require('../middleware');
 
 
 router.get("/:user_id", middleware.isUserAdmin, function(req, res){
+  var today = new Date();
+  var oneWeekAgo = new Date();
+  var oneMonthAgo = new Date();
+  var threeMonthAgo = new Date();
+
+  oneWeekAgo.setDate(oneWeekAgo.getDate() - 7); 
+  oneMonthAgo.setDate(oneMonthAgo.getDate() - 30);
+  threeMonthAgo.setDate(threeMonthAgo.getDate() - 90); 
+  console.log(typeof(oneWeekAgo.toString()));
+  console.log(typeof(oneMonthAgo.toString()));
+  console.log(typeof(threeMonthAgo.toString()));
   
   User.findById(req.params.user_id, function(err, foundUser){
     if (err) {
@@ -25,7 +36,7 @@ router.get("/:user_id", middleware.isUserAdmin, function(req, res){
             
             if(err){console.log(err);}
             Post.find({},
-            ['name','created','views', 'ranking','previousData'],
+            ['name','created','views', 'ranking'],
             {
                 skip:0, limit:10,
                 sort:{ views: -1 }
@@ -33,22 +44,25 @@ router.get("/:user_id", middleware.isUserAdmin, function(req, res){
               
               if(err){console.log(err);}
               Tags.find({},
-                ['tag','searchNumber','postsNumber','previousData'],
+                ['tag','searchNumber','postsNumber'],
                 {
                   skip:0, limit:10,
                   sort:{ searchNumber: -1 }
                 }, function(err, allTags) {
                   if(err){console.log(err);}
                   Tags.find({},
-                  ['tag','searchNumber','postsNumber','previousData'],
+                  ['tag','searchNumber','postsNumber'],
                   {
                     skip:0, limit:10,
                     sort:{ postsNumber: -1 }
                   }, function(err, tags_posts_data) {
-                    console.log(tags_posts_data);
                     if(err){console.log(err);}
-                      res.render("admin/profile", {user:foundUser, users:foundUsers, tags_data:allTags, posts_data: foundPosts, postsVotes: postsVotes, tags_posts_data: tags_posts_data});
-                  });
+                    Tags.find({ searchDate:{'$lte':new Date(today.toString()),'$gte': new Date(oneMonthAgo.toString()) } }, function(err, foundStats) {
+                      if(err){console.log(err);}
+                      console.log(foundStats);
+                      res.render("admin/profile", {user:foundUser, users:foundUsers, tags_data:allTags, posts_data: foundPosts, postsVotes: postsVotes, tags_posts_data: tags_posts_data, foundStats: foundStats});
+                  });    
+                });  
               });
             });  
           });  
@@ -56,7 +70,6 @@ router.get("/:user_id", middleware.isUserAdmin, function(req, res){
       });      
     }
   });
-  
 });
 
 module.exports = router;
